f793cd5e58927ec93536160d1658183722bcefd0
zerocold242 <alexrylexx@gmail.com>
2022-11-06 20:40:53 +0300

    Initial Commit

---
+++ SandBoxDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SandBoxDemo
+//
+//  Created by Aleksey Lexx on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
